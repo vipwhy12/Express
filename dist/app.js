9981dcd5d6767eb2552ddfc94cee2cb19b29a598
vipwhy12 <vipwhy12@naver.com>
2024-08-28 22:36:18 +0900

    FEAT: MongoDB 연결

--- dist/app.js
+++ dist/app.js
@@ -1,14 +1,41 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const dbPass = process.env.DB_PASS;
+const uri = `mongodb+srv://vipwhy12:${dbPass}@express.rm1fw.mongodb.net/?retryWrites=true&w=majority&appName=express`;
+const app = express();
 const port = 3000;
-app.get("/", function (req, res) {
-    res.send("Hello World");
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
 });
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+function run() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            // Connect the client to the server	(optional starting in v4.7)
+            yield client.connect();
+            // Send a ping to confirm a successful connection
+            yield client.db("admin").command({ ping: 1 });
+            console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        }
+        finally {
+            // Ensures that the client will close when you finish/error
+            yield client.close();
+        }
+    });
+}
+run().catch(console.dir);
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,33 @@
-import express from "express";
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
+const mongodbPassword = process.env.DB_PASS;
+const uri = `mongodb+srv://vipwhy12:${mongodbPassword}@express.rm1fw.mongodb.net/?retryWrites=true&w=majority&appName=express`;
+require("dotenv").config();
 const app = express();
-const port = 3000;
+const port: number = 3000;
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
